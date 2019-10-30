6e5ff4bb7f421701d059ebea3a4112dbfcfe360a
Blake Bovaird <cblakebovaird@gmail.com>
2019-10-30 01:23:14 -0400

    initial commit

---
+++ src/pages/Home.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import Jumbotron from "../components/Jumbotron";
+// import { Link } from "react-router-dom";
+import { Col, Row, Container } from "../components/Grid";
+
+class Home extends Component {
+    render() {
+        return (
+          <Container fluid>
+            <Row>
+              <Col size="md-6">
+                <Jumbotron>
+                  <h1>Home Page</h1>
+                </Jumbotron>
+                </Col>
+        </Row>
+      </Container>
+        );
+    }
+}
+
+export default Home;
